515be17be893b7c10ab6b3274cb3eb970c6a028b
Nick Coultas <nncoultas@yahoo.com>
2018-06-01 08:23:53 -0700

    Completed Task 1

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -101,6 +101,16 @@ int main(void)
         #endif
         
         /* Add your code for implementing the shell's logic here */
+        int rc = fork();
+
+       if (rc < 0) {
+           fprintf(stderr, "Child process failed.\n");
+           exit(1);
+       } else if (rc == 0) {
+           execvp(args[0], args);
+       } else {
+           int wc = waitpid(rc, NULL, 0);
+       }
         
     }
 
