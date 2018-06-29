e14bc74bf783e450fe6fe080063e423a69f49e3a
Justin Hong <jhong2009@gmail.com>
2018-06-29 10:55:11 -0700

    in lssh.c: implemented ability to change dirs

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 
 #define PROMPT "lambda-shell$ "
 
@@ -94,9 +95,9 @@ int main(void)
         // Some debugging output
         printf("args count %d\n", args_count);
         // Print out the parsed command line in args[]
-        // for (int i = 0; args[i] != NULL; i++) {
-        //     printf("%d: '%s'\n", i, args[i]);
-        // }
+        for (int i = 0; args[i] != NULL; i++) {
+            printf("%d: '%s'\n", i, args[i]);
+        }
 
         #endif
         
@@ -109,6 +110,15 @@ int main(void)
             exit(1);
         } else if (rc == 0) {
             // child process - run the new command here
+            if (strcmp(args[0], "cd") == 0) {
+              if (args_count == 2) {
+                int changeDir = chdir(args[1]);
+                if (changeDir < 0) {
+                  perror("chdir");
+                }
+                continue;
+              }
+            }
             execvp(args[0], args);
             printf("This should not be seen");
         } else {
