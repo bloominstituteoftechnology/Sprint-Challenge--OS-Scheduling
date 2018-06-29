5cee0f5f115d3f765e81ebbf68907583941092b6
hilal aissani <hilalaissani@gmail.com>
2018-06-29 15:53:02 -0400

    50%

---
+++ lssh/try1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  char jdir;
+  // printf("Enter job directory:"); /* user input for different directories */
+  // scanf("jdir");
+  // chdir("/home/jobs/%jdir/xxx");
+  system("ls -ltr");
+  return (0);
+}
