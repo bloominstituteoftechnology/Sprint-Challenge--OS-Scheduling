de9f24d9a5a51456432046e2fecb82dcb62b567b
SyCoDe <sycode@gmail.com>
2018-06-29 11:32:27 -0600

    Tasks 1 & 2 complete

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h> // error handling library
 
-#define PROMPT "lambda-shell$ "
+#define PROMPT "lambda-shell$   "
 
 #define MAX_TOKENS 100
 #define COMMANDLINE_BUFSIZE 1024
-#define DEBUG 1  // Set to 1 to turn on some debugging output, or 0 to turn off
+#define DEBUG 0 // Set to 1 to turn on some debugging output, or 0 to turn off
+
+// https://github.com/shiena/ansicolor/blob/master/README.md
+#define ANSI_FOREGROUND_BLUE "\x1b[34m"
+#define ANSI_FOREGROUND_WHITE "\x1b[37m"
+#define ANSI_BLINK_ON "\x1b[5m"
 
 /**
  * Parse the command line.
@@ -32,12 +38,13 @@
 char **parse_commandline(char *str, char **args, int *args_count)
 {
     char *token;
-    
+
     *args_count = 0;
 
     token = strtok(str, " \t\n\r");
 
-    while (token != NULL && *args_count < MAX_TOKENS - 1) {
+    while (token != NULL && *args_count < MAX_TOKENS - 1)
+    {
         args[(*args_count)++] = token;
 
         token = strtok(NULL, " \t\n\r");
@@ -63,46 +70,94 @@ int main(void)
     int args_count;
 
     // Shell loops forever (until we tell it to exit)
-    while (1) {
+    while (1)
+    {
         // Print a prompt
-        printf("%s", PROMPT);
+        char cwd[256];
+        getcwd(cwd, sizeof(cwd));
+        printf(ANSI_FOREGROUND_BLUE "%s", PROMPT);
+        printf(ANSI_FOREGROUND_WHITE "PWD = %s" ANSI_BLINK_ON ": ", cwd);
         fflush(stdout); // Force the line above to print
 
         // Read input from keyboard
         fgets(commandline, sizeof commandline, stdin);
 
         // Exit the shell on End-Of-File (CRTL-D)
-        if (feof(stdin)) {
+        if (feof(stdin))
+        {
             break;
         }
 
         // Parse input into individual arguments
         parse_commandline(commandline, args, &args_count);
 
-        if (args_count == 0) {
+        if (args_count == 0)
+        {
             // If the user entered no commands, do nothing
             continue;
         }
 
         // Exit the shell if args[0] is the built-in "exit" command
-        if (strcmp(args[0], "exit") == 0) {
+        if (strcmp(args[0], "exit") == 0)
+        {
             break;
         }
 
-        #if DEBUG
+#if DEBUG
 
         // Some debugging output
 
         // Print out the parsed command line in args[]
-        for (int i = 0; args[i] != NULL; i++) {
+        for (int i = 0; args[i] != NULL; i++)
+        {
             printf("%d: '%s'\n", i, args[i]);
         }
 
-        #endif
-        
+#endif
+
         /* Add your code for implementing the shell's logic here */
-        
+        int rc = fork();
+        if (rc < 0)
+        {
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        }
+        else if (rc == 0)
+        {
+            //cd
+            if (strcmp(args[0], "cd") == 0)
+            {
+                if (args_count < 2)
+                    printf("Please provide the directory name\n");
+                else
+                {
+                    int change = chdir(args[1]);
+                    if (change < 0)
+                        perror("chdir"); //Error check the result of chdir()
+                    continue;  //short-circuit the rest of the main loop
+                }
+            }
+            //&
+            else if (strcmp(args[args_count - 1], "&") == 0)
+            {
+                printf("wehitgold!");
+                args[args_count - 1] = NULL;
+                execvp(args[0], args);
+                printf("%s", PROMPT);
+                fflush(stdout);
+            }
+            //execute other commands
+            else
+                execvp(args[0], args);
+        }
+        else
+        {
+            int wc = waitpid(rc, NULL, 0);
+        }
     }
+
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        ;
 
     return 0;
 }
