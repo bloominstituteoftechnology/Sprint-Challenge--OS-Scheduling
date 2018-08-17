b2268415ab0c1aedb81c906846ab7a1ce7c552e9
Danny Vail <danny_vail@outlook.com>
2018-08-17 12:30:37 -0400

    Clean up main file In lssh.c - Removed unnecessary comments - Reformatted file

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -10,56 +10,33 @@
  */
 int main(void)
 {
-    // Holds the command line the user types in
-    char commandline[COMMANDLINE_BUFSIZE];
+  char commandline[COMMANDLINE_BUFSIZE];
+  char *args[MAX_TOKENS];
+  int args_count;
 
-    // Holds the parsed version of the command line
-    char *args[MAX_TOKENS];
+  while (1)
+  {
+    printf("%s", PROMPT);
+    fflush(stdout);
+    fgets(commandline, sizeof commandline, stdin);
 
-    // How many command line args the user typed
-    int args_count;
+    if (feof(stdin))
+      break;
 
-    // Shell loops forever (until we tell it to exit)
-    while (1) {
-        // Print a prompt
-        printf("%s", PROMPT);
-        fflush(stdout); // Force the line above to print
+    parse_commandline(commandline, args, &args_count);
 
-        // Read input from keyboard
-        fgets(commandline, sizeof commandline, stdin);
+    if (strcmp(args[0], "exit") == 0)
+      break;
+    if (args_count == 0)
+      continue;
 
-        // Exit the shell on End-Of-File (CRTL-D)
-        if (feof(stdin)) {
-            break;
-        }
+    #if DEBUG
+      for (int i = 0; args[i] != NULL; i++)
+        printf("%d: '%s'\n", i, args[i]);
+    #endif
 
-        // Parse input into individual arguments
-        parse_commandline(commandline, args, &args_count);
+    /* Add your code for implementing the shell's logic here */
+  }
 
-        if (args_count == 0) {
-            // If the user entered no commands, do nothing
-            continue;
-        }
-
-        // Exit the shell if args[0] is the built-in "exit" command
-        if (strcmp(args[0], "exit") == 0) {
-            break;
-        }
-
-        #if DEBUG
-
-        // Some debugging output
-
-        // Print out the parsed command line in args[]
-        for (int i = 0; args[i] != NULL; i++) {
-            printf("%d: '%s'\n", i, args[i]);
-        }
-
-        #endif
-        
-        /* Add your code for implementing the shell's logic here */
-        
-    }
-
-    return 0;
+  return 0;
 }
