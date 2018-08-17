5affd7a2e7cb80d1cbab4079bee74394a6cd9515
jesuarva <jesuarva.developer@gmail.com>
2018-08-17 19:30:55 +0200

    IMPLEMENT TASK-1

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -7,7 +7,7 @@
 
 #define MAX_TOKENS 100
 #define COMMANDLINE_BUFSIZE 1024
-#define DEBUG 1  // Set to 1 to turn on some debugging output, or 0 to turn off
+#define DEBUG 1 // Set to 1 to turn on some debugging output, or 0 to turn off
 
 /**
  * Parse the command line.
@@ -32,12 +32,13 @@
 char **parse_commandline(char *str, char **args, int *args_count)
 {
     char *token;
-    
+
     *args_count = 0;
 
     token = strtok(str, " \t\n\r");
 
-    while (token != NULL && *args_count < MAX_TOKENS - 1) {
+    while (token != NULL && *args_count < MAX_TOKENS - 1)
+    {
         args[(*args_count)++] = token;
 
         token = strtok(NULL, " \t\n\r");
@@ -63,7 +64,8 @@ int main(void)
     int args_count;
 
     // Shell loops forever (until we tell it to exit)
-    while (1) {
+    while (1)
+    {
         // Print a prompt
         printf("%s", PROMPT);
         fflush(stdout); // Force the line above to print
@@ -72,36 +74,59 @@ int main(void)
         fgets(commandline, sizeof commandline, stdin);
 
         // Exit the shell on End-Of-File (CRTL-D)
-        if (feof(stdin)) {
+        if (feof(stdin))
+        {
             break;
         }
 
         // Parse input into individual arguments
         parse_commandline(commandline, args, &args_count);
 
-        if (args_count == 0) {
+        if (args_count == 0)
+        {
             // If the user entered no commands, do nothing
             continue;
         }
 
         // Exit the shell if args[0] is the built-in "exit" command
-        if (strcmp(args[0], "exit") == 0) {
+        if (strcmp(args[0], "exit") == 0)
+        {
             break;
         }
 
-        #if DEBUG
+#if DEBUG
 
         // Some debugging output
 
         // Print out the parsed command line in args[]
-        for (int i = 0; args[i] != NULL; i++) {
+        for (int i = 0; args[i] != NULL; i++)
+        {
             printf("%d: '%s'\n", i, args[i]);
         }
 
-        #endif
-        
+#endif
+
         /* Add your code for implementing the shell's logic here */
-        
+        int forked_p = fork();
+
+        if (forked_p < 0)
+        {
+            printf("PARENT FORK failed.\n");
+        }
+        else if (forked_p == 0)
+        {
+            printf("=== %d CHILD_START fork1 ===\n", (int)getpid());
+            // We are going to get the     execl("whereis", "", ">", "whereis.txt", (char *)0);path to the binaries via the 'whereis' command.
+            execvp(args[0], args);
+            printf("=== CHILD_ERROR caling 'execvp' ===\n");
+            exit(1);
+        }
+        else
+        {
+            printf("=== %d PARENT_START ===\n", (int)getpid());
+            waitpid(forked_p, NULL, 0);
+            printf("=== PARENT_END ===\n");
+        }
     }
 
     return 0;
