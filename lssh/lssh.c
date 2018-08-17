ff5ac8857232936be6b7bf58f8f93924d2f36a37
jesuarva <jesuarva.developer@gmail.com>
2018-08-17 19:34:24 +0200

    IMPLEMENT TASK-2

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -94,6 +94,12 @@ int main(void)
             break;
         }
 
+        if (strcmp(args[0], "cd") == 0)
+        {
+            chdir(args[1]);
+            continue;
+        }
+
 #if DEBUG
 
         // Some debugging output
