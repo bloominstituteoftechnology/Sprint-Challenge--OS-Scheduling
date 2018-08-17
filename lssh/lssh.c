e20a7f04223321b615c39b5d9f84f903678e2291
mister-corn <code@ronaldlibago.com>
2018-08-16 17:31:24 -0700

    T+2hr3min: `&` argument is now being handled.
    * May be buggy, so will keep tabs at it as I move forward.

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -11,6 +11,131 @@
 #define COMMANDLINE_BUFSIZE 1024
 #define DEBUG 1  // Set to 1 to turn on some debugging output, or 0 to turn off
 
+// My little baby functions. Aww, so cute.
+char **parse_commandline(char *str, char **args, int *args_count);
+void doCD(char** args, int args_count);
+void flagSpecialArgs(char** args, int args_count, int *background, int *redirection);
+
+/**
+ * Main
+ */
+int main(void)
+{
+    // Holds the command line the user types in
+    char commandline[COMMANDLINE_BUFSIZE];
+
+    // Holds the parsed version of the command line
+    char *args[MAX_TOKENS];
+
+    // How many command line args the user typed
+    int args_count;
+
+    // Shell loops forever (until we tell it to exit)
+    while (1) {
+        // Command modifier flags
+        int background = 0;
+        int redirection = 0;
+
+        // Clear out the zombies
+        while (waitpid(-1, NULL, WNOHANG) > 0)
+            ;
+
+        // Print a prompt
+        printf("%s", PROMPT);
+        fflush(stdout); // Force the line above to print
+
+        // Read input from keyboard
+        fgets(commandline, sizeof commandline, stdin);
+
+        // Exit the shell on End-Of-File (CRTL-D)
+        if (feof(stdin)) {
+            break;
+        }
+
+        // Parse input into individual arguments
+        parse_commandline(commandline, args, &args_count);
+
+        if (args_count == 0) {
+            // If the user entered no commands, do nothing
+            continue;
+        }
+
+        // Exit the shell if args[0] is the built-in "exit" command
+        if (strcmp(args[0], "exit") == 0) {
+            break;
+        }
+
+        #if DEBUG
+
+        // Some debugging output
+
+        // Print out the parsed command line in args[]
+        for (int i = 0; args[i] != NULL; i++) {
+            printf("%d: '%s'\n", i, args[i]);
+        }
+
+        #endif
+        
+        flagSpecialArgs(args, args_count, &background, &redirection);
+
+        /* Add your code for implementing the shell's logic here */
+        int rc = fork();
+
+        if (rc < 0) {
+            fprintf(stderr, "Fork unsuccessful. Perhaps try using a knife?\n");
+            exit(1);
+        }
+        else if (rc == 0) {
+            // If the command is `cd`, we'll take care of it instead of exec()
+            if (strcmp(args[0], "cd") == 0) {
+                doCD(args, args_count);
+                continue;
+            }
+            //
+            // The actual execution of the program
+            execvp(args[0], args);
+        }
+        else {
+            printf("PARENT background %d\n", background);
+            if (!background) {
+                printf("BOO!\n");
+                waitpid(rc, NULL, 0);
+            }
+        }
+    }
+
+    return 0;
+}
+
+void doCD(char** args, int args_count)
+{
+    if (args_count != 2) {
+        fprintf(stderr, "usage: cd <directory>\n");
+    }
+    else {
+        // Execution is in the if conditional
+        // Error handling accomplished by checking its return value
+        if (chdir(args[1]) == -1) {
+            perror("chdir");
+        }
+    }
+}
+
+void flagSpecialArgs(char* args[], int args_count, int *background, int *redirection)
+{
+    int lastCharInd = args_count - 1;
+
+    if (args[lastCharInd][0] == '&') {
+        *background = 1;
+        args[lastCharInd] = NULL;
+    }
+    // if (args[lastCharInd][0] == '>') {
+    //     *redirection = 1;
+    //     args[lastCharInd] = NULL;
+    // }
+    
+}
+
 /**
  * Parse the command line.
  *
@@ -48,87 +173,4 @@ char **parse_commandline(char *str, char **args, int *args_count)
     args[*args_count] = NULL;
 
     return args;
-}
-
-/**
- * Main
- */
-int main(void)
-{
-    // Holds the command line the user types in
-    char commandline[COMMANDLINE_BUFSIZE];
-
-    // Holds the parsed version of the command line
-    char *args[MAX_TOKENS];
-
-    // How many command line args the user typed
-    int args_count;
-
-    // Shell loops forever (until we tell it to exit)
-    while (1) {
-        // Print a prompt
-        printf("%s", PROMPT);
-        fflush(stdout); // Force the line above to print
-
-        // Read input from keyboard
-        fgets(commandline, sizeof commandline, stdin);
-
-        // Exit the shell on End-Of-File (CRTL-D)
-        if (feof(stdin)) {
-            break;
-        }
-
-        // Parse input into individual arguments
-        parse_commandline(commandline, args, &args_count);
-
-        if (args_count == 0) {
-            // If the user entered no commands, do nothing
-            continue;
-        }
-
-        // Exit the shell if args[0] is the built-in "exit" command
-        if (strcmp(args[0], "exit") == 0) {
-            break;
-        }
-
-        #if DEBUG
-
-        // Some debugging output
-
-        // Print out the parsed command line in args[]
-        for (int i = 0; args[i] != NULL; i++) {
-            printf("%d: '%s'\n", i, args[i]);
-        }
-
-        #endif
-        
-        /* Add your code for implementing the shell's logic here */
-        int rc = fork();
-
-        if (rc < 0) {
-            fprintf(stderr, "Fork unsuccessful. Perhaps try using a knife?\n");
-            exit(1);
-        }
-        else if (rc == 0) {
-            // If the command is `cd`, we'll take care of it instead of exec()
-            if (strcmp(args[0], "cd") == 0) {
-                if (args_count != 2) {
-                    fprintf(stderr, "usage: cd <directory>\n");
-                }
-                else {
-                   if (chdir(args[1]) == -1) {
-                       perror("chdir");
-                   }
-                }
-                continue;
-            }
-            execvp(args[0], args);
-        }
-        else {
-            waitpid(rc, NULL, 0);
-        }
-        
-    }
-
-    return 0;
 }
