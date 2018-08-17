b6add5bc7644471408c51e8e8eda30e66d8e4ccd
Danny Vail <danny_vail@outlook.com>
2018-08-17 12:42:40 -0400

    Defined COMMAND_NOT_FOUND constant In constants.h - Defined message to be displayed when exec() returns an error

--- lssh/constants.h
+++ lssh/constants.h
@@ -1,6 +1,7 @@
 #ifndef constants_h
 
 #define PROMPT "lambda-shell$ "
+#define COMMAND_NOT_FOUND "lambda-shell$ Command not found\n"
 #define MAX_TOKENS 100
 #define COMMANDLINE_BUFSIZE 1024
 #define DEBUG 1
