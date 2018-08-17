c33174eac85a3bfbdf586e6e57832c0e9230c9a1
Danny Vail <danny_vail@outlook.com>
2018-08-17 12:57:22 -0400

    Defined CD_ERROR In constants.h - Defined error message for changing directories

--- lssh/constants.h
+++ lssh/constants.h
@@ -2,6 +2,7 @@
 
 #define PROMPT "lambda-shell$ "
 #define COMMAND_NOT_FOUND "lambda-shell$ Command not found\n"
+#define CD_ERROR "lambda-shell$ No such file or directory\n"
 #define MAX_TOKENS 100
 #define COMMANDLINE_BUFSIZE 1024
 #define DEBUG 1
