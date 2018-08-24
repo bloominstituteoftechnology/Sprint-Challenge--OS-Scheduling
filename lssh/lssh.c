523bae2524769b8408abb7df041716360c8e0b5a
Tramane Hall <tramanedhall@gmail.com>
2018-08-24 13:25:41 -0400

    added logic for lssh.c

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -101,6 +101,104 @@ int main(void)
         #endif
         
         /* Add your code for implementing the shell's logic here */
+        if (args_count == 2 && strcmp(args[0], "cd") == 0) {
+                int cd = chdir(args[1]);
+                continue;
+                if (cd == -1){
+                    perror("chdir");
+                }            
+            }
+
+        char *redir_outfile = NULL;
+        int fd[2];
+        
+        for (int i = 0; i < args_count; i++) {
+            if (strcmp(args[i], ">") == 0){
+                redir_outfile = args[i+1];
+                args[i] = NULL;
+            }
+            else if (strcmp(args[i], "|") == 0) {
+                if (pipe(fd) < 0) {
+                    fprintf(stderr, "pipe failed\n");
+                    exit(4);
+                }
+                else {
+                  int pipefork = fork();
+
+                  if (pipefork < 0) {
+                    fprintf(stderr, "Fork failed");
+                    exit(5);
+                  }
+                  else if (pipefork == 0) {
+                    dup2(fd[0], 0);
+                    close(fd[1]);
+                    execlp(args[args_count-2], args[args_count-2], args[args_count-1], NULL);
+                    
+                    perror("exec child");
+                    exit(6);
+                  }
+                  else {
+                    dup2(fd[1], 1);
+                    close(fd[0]);
+
+                    execlp(args[0], args[0], args[1], NULL);
+                    
+                    perror("exec parent");
+                    exit(7);
+                  }
+                }
+            }
+        }
+        if (strcmp(args[(args_count-1)], "&") == 0) {
+            args[args_count-1] = NULL;
+            
+            pid_t background = fork();
+            if (background < 0){
+                fprintf(stderr, "Background fork failed. \n");
+                exit(2);
+            }
+            else if (background == 0){
+                if (redir_outfile) {
+                int fd = open(redir_outfile, O_CREAT|O_WRONLY, 0644);
+
+                    if (fd < 0){
+                        fprintf(stderr, "Failed to open file: %s", redir_outfile);
+                        continue;
+                        }
+                    else {
+                        dup2(fd, 1);
+                        }
+                    }
+                if (execvp(args[0], args) == -1){
+                    fprintf(stderr, "Background process failed to execute.");
+                }
+            }
+        }   
+        else {
+            pid_t rc = fork();
+            if (rc < 0) {
+                fprintf(stderr, "Fork failed. \n");
+                exit(1);
+                }
+            else if (rc == 0) {
+                if (redir_outfile) {
+                    int fd = open(redir_outfile, O_CREAT|O_WRONLY, 0644);
+                    if (fd < 0){
+                        fprintf(stderr, "Failed to open file: %s", redir_outfile);
+                        continue;
+                        }
+                    else {
+                        dup2(fd, 1);
+                    }
+                }
+                if (execvp(args[0], args) == -1){
+                    fprintf(stderr, "Child command failed to execute.");
+                    }
+                }
+            else {
+                wait(NULL);
+            }
+        }   
         
     }
 
