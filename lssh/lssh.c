28e76139c6078ab61fe20cf1c8a823686d76df19
Tramane Hall <tramanedhall@gmail.com>
2018-08-24 13:31:20 -0400

    commented to help better understand the program and some refactoring

--- lssh/lssh.c
+++ lssh/lssh.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 #define PROMPT "lambda-shell$ "
 
@@ -35,17 +39,41 @@ char **parse_commandline(char *str, char **args, int *args_count)
     
     *args_count = 0;
 
-    token = strtok(str, " \t\n\r");
+    token = strtok(str, " \t\n\r"); 
+    /* takes entire string: delimeters are tab, new line, return
+    token is a pointer to the first token found in the string if token found
+    token is a null pointer if no tokens found */
 
-    while (token != NULL && *args_count < MAX_TOKENS - 1) {
-        args[(*args_count)++] = token;
+    while (token != NULL && *args_count < MAX_TOKENS - 1) { 
+        // if null pointer, does not do while loop
+        // stop while loop if args_count gets larger than or equal to MAX_TOKENS - 1
+        args[(*args_count)++] = token; 
+        // the element in args at index args_count is now the token pointer
+        // increment args_count
+        // args_count started at 0
 
-        token = strtok(NULL, " \t\n\r");
+        token = strtok(NULL, " \t\n\r"); 
+        /* loops through the string token is pointing to (of tokens)
+        and if it finds a delimiter, replaces it with a null byte to terminate the current token
+        strtok then saves the following byte which will be the pointer to start searching for next token */
     }
 
-    args[*args_count] = NULL;
+    args[*args_count] = NULL; // sets the element at args_count to null in args
 
     return args;
+}
+
+static void sigchld_hdl (int sig)
+{
+    (void)sig;
+    int saved_errno = errno; 
+    // waitpid() might overright errno, so this saves and restores it
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+    // wait for any child process
+    // do not store information
+    // WNOHANG specifies that it should return immediately if there are 
+    // no child processes exited
+    errno = saved_errno;
 }
 
 /**
@@ -57,22 +85,35 @@ int main(void)
     char commandline[COMMANDLINE_BUFSIZE];
 
     // Holds the parsed version of the command line
-    char *args[MAX_TOKENS];
+    char *args[MAX_TOKENS]; // char *args[100]
 
     // How many command line args the user typed
     int args_count;
+    struct sigaction bg;
 
     // Shell loops forever (until we tell it to exit)
     while (1) {
+        bg.sa_handler = sigchld_hdl; // action associated with SIGCHLD
+        sigemptyset(&bg.sa_mask); // this blocks other terminal-generated signals while this handler runs
+        bg.sa_flags = SA_RESTART; // restarts the signal handler if it is interrupted
+        if (sigaction(SIGCHLD, &bg, NULL) == -1) {
+            // sigaction system call changes the action taken by process 
+            // on receipt of specific signal (this case SIGCHLD)
+            perror("sigaction");
+            exit(3);
+        }
         // Print a prompt
         printf("%s", PROMPT);
         fflush(stdout); // Force the line above to print
 
         // Read input from keyboard
         fgets(commandline, sizeof commandline, stdin);
+        // reads from stdin and stores it in commandline to the maximum of 1024
 
         // Exit the shell on End-Of-File (CRTL-D)
+        // CTRL-D is translated as end of standard input
         if (feof(stdin)) {
+            printf("\n");
             break;
         }
 
@@ -97,6 +138,7 @@ int main(void)
         for (int i = 0; args[i] != NULL; i++) {
             printf("%d: '%s'\n", i, args[i]);
         }
+        printf("Number of args: %d\n", args_count);
 
         #endif
         
@@ -197,10 +239,9 @@ int main(void)
                 }
             else {
                 wait(NULL);
+                }
             }
-        }   
-        
-    }
+        }
 
     return 0;
 }
